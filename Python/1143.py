b298821dca82e87787091264c7d740c0c699cf6a
JWang169 <lo_vegood@126.com>
2020-04-26 14:26:58 -0400

    :package:

--- Python/1143.py
+++ Python/1143.py
@@ -1,3 +1,25 @@
+# April 26 3rd
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(m):
+            for j in range(n):
+                if text1[i] == text2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + 1 
+                else:
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
+        return dp[-1][-1]
+        
+        
+
+
 # April 2nd
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:      
